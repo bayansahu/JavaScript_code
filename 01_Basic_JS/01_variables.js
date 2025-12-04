6aa7fbb20a694fd00f1c45e19b3c68d51e402ee0
Bayan Kumar Sahu <117112875+bayansahu@users.noreply.github.com>
2025-12-04 15:41:48 +0000

    2_notes added

---
+++ 01_Basic_JS/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let email = "Bayan@gmail.com"
+var accountPassword = "Bayan@12345"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId, email, accountPassword, accountCity])
---
+++ 01_Basic_JS/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"// treat all code as newer javaScript version
+
+let name = "Bayan"       // string
+let roll = 2             // number
+let islogedin = true     // boolean
+let email;               // undefined
+let id = null            // Null - standalone value
+
+let num = 23456789084n  // bigInt
+// Symbol - Unique
+// Object
+
+console.log(typeof num);
+
+
+// Coversation of Data Types
+let value = '345' // datatype is string
+
+let s = Number(value); // the datatype of value is changed from string to number
+
+console.log(typeof s);
+
